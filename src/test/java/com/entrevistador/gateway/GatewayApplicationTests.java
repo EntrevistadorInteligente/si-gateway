405837e90468ed21be406cc58a875d021f980384
DylanSerkovich <n00189659@upn.pe>
2024-08-25 01:04:05 -0500

    supress warning

--- src/test/java/com/entrevistador/gateway/GatewayApplicationTests.java
+++ src/test/java/com/entrevistador/gateway/GatewayApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@SuppressWarnings("java:S116")
 @TestPropertySource("classpath:test-application.properties")
 class GatewayApplicationTests {
 
